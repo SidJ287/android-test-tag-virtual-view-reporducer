cb79aee0e3042af30062758ae73585be3fac9103
Siddhant Jain <siddhant.s@zuper.co>
2024-10-13 19:59:47 +0530

    add button

--- app/src/main/java/com/example/virtualviewtesttag/ComposeButtonActivity.kt
+++ app/src/main/java/com/example/virtualviewtesttag/ComposeButtonActivity.kt
@@ -4,16 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTagsAsResourceId
 import androidx.compose.ui.unit.dp
@@ -41,7 +47,21 @@ class ComposeButtonActivity : ComponentActivity() {
                     modifier = Modifier.testTag("COMPOSE_BUTTON_TAG"),
                     onClick = {},
                 ) {
-                    Text(text = "Compose Buttons with Semantics ")
+                    Text(text = "Compose Buttons")
+                }
+
+                Spacer(modifier = Modifier.size(16.dp))
+
+                Surface(
+                    modifier = Modifier
+                        .clickable(role = Role.Button) {}
+                        .testTag("COMPOSE_CLICKABLE_ROW"),
+                    color = Color.Red,
+                ) {
+                    Text(
+                        modifier = Modifier.padding(16.dp),
+                        text = "Clickable Row",
+                    )
                 }
             }
         }
